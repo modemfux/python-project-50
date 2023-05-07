dadd4256feb3bdbcadfb57fc27974c3cf68ebdb8
modemfux <modemfux@gmail.com>
2023-05-07 22:36:25 +0300

    Corrected check function and test suit

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,20 +1,18 @@
-
 import json
 
 
 def check_key_in_dicts(key, dict1, dict2):
-    keys = {
-        '0': '  ' + key,
-        '1': '- ' + key,
-        '2': '+ ' + key
-        }
-    if key in dict1.keys() and key in dict2.keys():
-        return keys['0'], dict1[key] if dict1[key] == dict2[key] else (
-            keys['1'], dict1[key], keys['2'], dict2[key])
-    elif key in dict1.keys() and key not in dict2.keys():
-        return keys['1'], dict1.get(key)
-    elif key in dict2.keys() and key not in dict1.keys():
-        return keys['2'], dict2.get(key)
+    key0 = '  ' + key
+    key1 = '- ' + key
+    key2 = '+ ' + key
+    if key in dict1 and key in dict2 and dict1[key] == dict2[key]:
+        return key0, dict1[key]
+    elif key in dict1 and key in dict2 and dict1[key] != dict2[key]:
+        return (key1, dict1[key], key2, dict2[key])
+    elif key in dict1 and key not in dict2:
+        return key1, dict1.get(key)
+    elif key in dict2 and key not in dict1:
+        return key2, dict2.get(key)
 
 
 def generate_diff(file1, file2):
@@ -26,11 +24,11 @@ def generate_diff(file1, file2):
     new_dict = {}
     for key in all_keys:
         mid_res = check_key_in_dicts(key, item1, item2)
-        if not isinstance(mid_res[1], tuple):
+        if len(mid_res) == 2:
             n_key, n_value = mid_res
             new_dict[n_key] = n_value
         else:
-            n_key1, n_value1, n_key2, n_value2 = mid_res[1]
+            n_key1, n_value1, n_key2, n_value2 = mid_res
             new_dict[n_key1] = n_value1
             new_dict[n_key2] = n_value2
     return json.dumps(new_dict, indent=4).replace('"', '')
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -46,13 +46,11 @@ def test_check_key_in_dicts(keys, dict1, dict2):
     assert check_key_in_dicts(key1, dict1, dict2) == ('  host', 'hexlet.io')
     assert check_key_in_dicts(key2, dict1, dict2) == ('- proxy',
                                                       '123.234.53.22')
-    assert check_key_in_dicts(key3, dict1, dict2) == ('  timeout',
-                                                      ('- timeout', 50,
-                                                       '+ timeout', 20))
+    assert check_key_in_dicts(key3, dict1, dict2) == ('- timeout', 50,
+                                                      '+ timeout', 20)
     assert check_key_in_dicts(key4, dict1, dict2) == ('+ verbose', True)
 
 
 def test_generate_diff(files, expected_output):
     file1, file2 = files
     assert generate_diff(file1, file2) == expected_output
-
