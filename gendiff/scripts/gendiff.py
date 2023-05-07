829924595cdc67fc08276ec47e929caaeb89d31a
modemfux <modemfux@gmail.com>
2023-05-07 22:57:09 +0300

    Trying to increase maintainability

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -8,10 +8,8 @@ def check_key_in_dicts(key, dict1, dict2):
         '2': '+ ' + key
         }
     if key in dict1.keys() and key in dict2.keys():
-        if dict1[key] == dict2[key]:
-            return (keys['0'], dict1[key])
-        else:
-            return (keys['1'], dict1[key], keys['2'], dict2[key])
+        return (keys['0'], dict1[key]) if dict1[key] == dict2[key] else (
+            keys['1'], dict1[key], keys['2'], dict2[key])
     elif key in dict1.keys() and key not in dict2.keys():
         return keys['1'], dict1.get(key)
     elif key in dict2.keys() and key not in dict1.keys():
